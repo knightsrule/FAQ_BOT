e3725ec3e568148d75e3530f5157dc81b6ceefd9
Vijay Verma <vijayverma@VVMBP.local>
2023-02-26 10:18:35 -0500

    scrapy version of crawler

--- crawl.py
+++ crawl.py
@@ -1,13 +1,18 @@
-from collections import deque
-import json
-from os import path
 import os
 import re
-from urllib.parse import urlparse
-from datetime import datetime, timedelta
+import json
+import time
+from os import path
+from collections import deque
 from bs4 import BeautifulSoup
 from selenium import webdriver
+from urllib.parse import urlparse
+from datetime import datetime, timedelta
+from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
 
 # Regex pattern to match a URL
 HTTP_URL_PATTERN = r'^http[s]*://.+'
@@ -28,12 +33,25 @@ else:
 # Parse the URL and get the domain
 local_domain = urlparse(full_url).netloc
 
+# remove new line from the text
+
+
+def remove_newlines(serie):
+    serie = serie.replace('\n', ' ')
+    serie = serie.replace('\\n', ' ')
+    serie = serie.replace('  ', ' ')
+    serie = serie.replace('  ', ' ')
+    return serie
+
 # Function to get html of a URL
 
 
 def fetch_url_selenium(driver, url):
-
     driver.get(url)
+    WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.TAG_NAME, 'body')))
+    WebDriverWait(driver, 10).until(lambda driver: driver.execute_script(
+        'return document.readyState') == 'complete')
     html = driver.page_source
     return html
 
@@ -120,7 +138,8 @@ def crawl(url):
         os.mkdir("processed/" + local_domain + "/")
 
     options = Options()
-    options.add_argument("--headless")
+    # options.add_argument("--headless")
+    options.add_argument("--enable-javascript")
     driver = webdriver.Chrome(options=options)
 
     # While the queue is not empty, continue crawling
@@ -140,16 +159,31 @@ def crawl(url):
             # Get the text from the URL using BeautifulSoup
             soup = BeautifulSoup(html, "html.parser")
 
+            # Extract the header
+            header = soup.find("header").prettify()
+
+            # Extract the body
+            body = soup.find("body").prettify()
+
+            # Extract the footer
+            footer = soup.find("footer").prettify()
+
+            # Print the results
+            print("Header:\n", len(header))
+            print("Body:\n", len(body))
+            print("Footer:\n", len(footer))
+
             # Get the text but remove the tags
-            text = soup.get_text()
+            text = soup.find("body").get_text()
+            text = remove_newlines(text)
 
             # Save text from the url to a <url>.txt file
             with open('text/'+local_domain+'/'+url[8:].replace("/", "_") + ".txt", "w", encoding="UTF-8") as f:
 
                 # If the crawler gets to a page that requires JavaScript, it will stop the crawl
-                if ("You need to enable JavaScript to run this app." in text):
-                    print("Unable to parse page " + url +
-                          " due to JavaScript being required")
+                # if ("You need to enable JavaScript to run this app." in text):
+                #    print("Unable to parse page " + url +
+                #          " due to JavaScript being required")
 
                 # Otherwise, write the text to the file in the text directory
                 f.write(text)
@@ -159,11 +193,14 @@ def crawl(url):
                 if link not in seen:
                     queue.append({'url': link, 'level': level + 1})
                     seen.add(link)
+
+            # sleep for one second to not overwhelm the site
+            time.sleep(1)
         else:
             print(url + ' at level ' + str(level) + ' is too deep')
 
     # terminate the selenium driver
-    driver.quit()
+    # driver.quit()
 
 
 crawl(full_url)
---
+++ crawlScrapy.py
@@ -0,0 +1,197 @@
+import json
+import os
+import re
+import scrapy
+from pathlib import Path
+from urllib.parse import urlparse
+from scrapy.selector import Selector
+from urllib.parse import urlparse
+from bs4 import BeautifulSoup
+from scrapy.crawler import CrawlerProcess
+import argparse
+from scrapy.crawler import CrawlerProcess
+from scrapy.utils.project import get_project_settings
+from scrapy.utils.log import configure_logging
+import logging
+
+
+class SiteDownloadSpider(scrapy.Spider):
+    name = "download"
+    MAX_DEPTH = 3
+    BASE_URL = ''
+
+    # Regex pattern to match a URL
+    HTTP_URL_PATTERN = r'^http[s]*://.+'
+
+    def __init__(self, *args, **kwargs):
+        super(SiteDownloadSpider, self).__init__(*args, **kwargs)
+
+        self.MAX_DEPTH = int(getattr(self, 'depth', 3))
+        self.BASE_URL = getattr(self, 'url', '')
+
+        print("in the constructor: ", self.BASE_URL, self.MAX_DEPTH)
+        self.visited_links = set()
+
+    # replace all occurences of src string in text with dest
+    def replaceAll(self, text, src, dest):
+        while src in text:
+            text = text.replace(src, dest)
+
+        return text
+
+    def remove_newlines(self, text):
+
+        text = text.strip()
+        text = text.replace('\r', '')
+        text = text.replace('\t', ' ')
+
+        text = self.replaceAll(text, '\n\n', '\n')
+        text = self.replaceAll(text, '  ', ' ')
+
+        return text
+
+    def get_domain_hyperlinks(self, response, root_domain):
+
+        clean_links = []
+
+        # Find all hyperlinks on the webpage and follow them
+        for link in response.xpath('//a/@href'):
+
+            # link_url = response.urljoin(link.extract())
+            link_url = link.extract()
+            # print(link, link_url)
+            clean_link = None
+
+            # If the link is a URL, check if it is within the same domain
+            if re.search(self.HTTP_URL_PATTERN, link_url):
+                # Parse the URL and check if the domain is the same
+                url_obj = urlparse(link_url)
+                if url_obj.netloc == root_domain:
+                    clean_link = link_url
+
+            # If the link is not a URL, check if it is a relative link
+            else:
+                if link_url.startswith("/"):
+                    link_url = link_url[1:]
+                elif link_url.startswith("#") or link_url.startswith("mailto:") or link_url.startswith("tel:"):
+                    continue
+                clean_link = "https://" + root_domain + "/" + link_url
+
+            if clean_link is not None:
+                if clean_link.endswith("/"):
+                    clean_link = clean_link[:-1]
+                clean_links.append(clean_link)
+
+        # Return the list of hyperlinks that are within the same domain
+        return list(set(clean_links))
+
+    def start_requests(self):
+
+        if self.BASE_URL:
+            # Define root domain to crawl
+            full_url = self.BASE_URL
+            # Parse the URL and get the domain
+            local_domain = urlparse(full_url).netloc
+
+            # Create a directory to store the text files
+            if not os.path.exists("text/"):
+                os.mkdir("text/")
+
+            if not os.path.exists("text/"+local_domain+"/"):
+                os.mkdir("text/" + local_domain + "/")
+
+            yield scrapy.Request(url=full_url, callback=self.parse, meta={'depth': 1, 'base_url': full_url})
+        else:
+            print('no base url found')
+
+    def parse(self, response):
+        # if response.url in self.visited_links:
+        #    return
+        # else:
+        #    self.visited_links.add(response.url)
+
+        base_url = response.meta.get('base_url', response.url)
+        base_domain = urlparse(base_url).netloc
+        url = response.url
+
+        depth = response.meta.get('depth', 0)
+        if depth > self.MAX_DEPTH:
+            print(url, ' at depth ', depth, " is too deep")
+            return
+        else:
+            print("processing: ", url)
+
+        # Save html from the url to a <url>.html file
+        # with open('text/'+base_domain+'/'+url[8:].replace("/", "_") + ".html", "w", encoding="UTF-8") as f:
+        #    f.write(response.body.decode("utf-8"))
+
+        # Save text from the url to a <url>.txt file
+        with open('text/'+base_domain+'/'+url[8:].replace("/", "_") + ".txt", "w", encoding="UTF-8") as f:
+
+            # Print the header of the page
+            # header = response.xpath('//head').get()
+            # f.write(f'Header of {response.url}:\n{header}\n\n')
+
+            # Print the header of the page
+            title = response.xpath('//head/title/text()').get()
+            f.write(f'{title}\n\n')
+
+            # Print the footer of the page
+            # footer = response.xpath('//footer').get()
+            # f.write(f'Footer of {response.url}:\n{footer}\n\n')
+
+            # Print the plain text version of the body of the page
+            body = scrapy.Selector(response).xpath('//body').getall()
+            soup = BeautifulSoup(''.join(body), 'html.parser')
+            # body_text = ''.join([text.strip() for text in body])
+            body_text = self.remove_newlines(soup.get_text())
+            # body_text = ''.join(response.selector.select(
+            #    "//body//text()").extract()).strip()
+
+            f.write(body_text)
+
+        # get links from the current page
+        subLinks = self.get_domain_hyperlinks(response, base_domain)
+
+        # tee up new links for traversal
+        for link in subLinks:
+            if link not in self.visited_links:
+                print("New link found: ", link)
+                self.visited_links.add(link)
+                yield scrapy.Request(url=link, callback=self.parse, meta={'depth': depth + 1, 'base_url': base_url})
+            else:
+                print("Previously visited link: ", link)
+
+
+# Define the command line arguments
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "url", help="root url of the site that needs to be analyzed")
+parser.add_argument(
+    "-depth", '--depth', help="how deep should the site be analyzed. default is 3", required=False)
+parser.add_argument('-log_level', "--log_level", help="set log level",
+                    choices=["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"])
+
+# Parse the command line arguments
+args = parser.parse_args()
+
+url = args.url
+depth = args.depth or 3
+log_level = args.log_level or get_project_settings().get("LOG_LEVEL")
+print("params are: ", url, depth, log_level)
+
+logging.getLogger().setLevel(log_level)
+configure_logging({'LOG_LEVEL': log_level})
+
+print('new log level: ', get_project_settings().get("LOG_LEVEL"))
+
+# Create a new Scrapy process
+process = CrawlerProcess()
+
+# process = CrawlerProcess({
+#    'USER_AGENT': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)'
+# })
+
+process.crawl(SiteDownloadSpider, input='inputargument', url=url, depth=depth)
+
+process.start()  # the script will block here until the crawling is finished
