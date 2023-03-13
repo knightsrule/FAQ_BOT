ca40ec8ff09e5678cfe22e9ec2073fcffd4ba160
Vijay Verma <vijayverma@WP2MC02DC14XMD6P.local>
2023-03-13 13:09:22 -0400

    add api endpoints

--- apig.py
+++ apig.py
@@ -13,7 +13,8 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)  # initialize the CORS extension
 
-start_url = 'https://www.mhs.net/'
+start_url = 'https://www.mower.com'
+start_url = 'https://www.televox.com'
 
 # Parse the URL and get the domain
 local_domain = urlparse(start_url).netloc
